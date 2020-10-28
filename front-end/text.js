462b1788302352e5750ddfdc0e23b52f8ffa38e4
thin nguyen <thinnguyen1888@gmail.com>
2020-10-28 21:09:27 +0700

    typescript

---
+++ front-end/text.js
@@ -0,0 +1,4 @@
+var years = [2014, 2015, 2016];
+for (var i = 0; i <= years.length; i++) {
+    console(years[i]);
+}
---
+++ front-end/text.ts
@@ -0,0 +1,4 @@
+let years: Array<number>=[2014,2015,2016];
+for(let i=0;i<=years.length;i++){
+console.log(years[i])
+}
